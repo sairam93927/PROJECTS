9a4568c400b179c574c0ca9535c166355090ecfb
sairam93927 <sairam93927@gmail.com>
2024-12-18 23:01:35 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter the 1st Number:"))
+num2=int(input("Enter the 2nt Number:"))
+operator=input("Give operator:")
+if operator=="+":
+    print(f"adiition of two numbers is {num1+num2}")
+elif operator=="-":
+     print(f"subtraction  of two numbers is{num1-num2}")
+elif operator=="*":
+    print(f"multiplication of two numbers is {num1*num2}")
+elif operator=="/":
+    print(f"division of two numbers is {num1*num2}") 
+else:
+    print("Invalid number")

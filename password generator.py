dfebec84859b7f675a065728a0f24a33faf02da8
sairam93927 <sairam93927@gmail.com>
2024-12-19 12:26:35 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -3,10 +3,10 @@ import random
 letters=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 numbers=['1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols=['!', '@', '#', '$', '^', '&', '*']
-print("welcome to password genrator")
-n_letters=int(input("how many letters you want in your password"))
-n_numbers=int(input("how many nubers you want in your password"))
-n_symbols=int(input("how many symbols you want in your password"))
+print("welcome to password genrator!!")
+n_letters=int(input("how many letters you want in your password:"))
+n_numbers=int(input("how many numbers you want in your password:"))
+n_symbols=int(input("how many symbols you want in your password:"))
 password=""
 for i in range (1,n_letters):
     char=random.choice(letters)
